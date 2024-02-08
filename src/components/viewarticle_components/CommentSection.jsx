067f4d6db56e57184e1965b1e1227444d29bf122
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-02-08 09:52:47 +0000

    add functionality to post comments on articles

--- src/components/viewarticle_components/CommentSection.jsx
+++ src/components/viewarticle_components/CommentSection.jsx
@@ -1,35 +1,53 @@
 import { useState, useEffect } from "react";
 import { fetchArticleComments } from "../../utils/apiRequest";
 import CommentCard from "./CommentCard";
+import CommentForm from "./CommentForm";
 
-const CommentSection = ({article_id}) => {
+const CommentSection = ({ article_id }) => {
   const [comments, setComments] = useState([]);
-  const [isLoading, setIsLoading] = useState(true)
-  const [showComments, setShowComments] = useState("Show Comments")
+  const [isLoading, setIsLoading] = useState(true);
+  const [showComments, setShowComments] = useState("Show Comments");
+  const [triggerFetch, setTriggerFetch] = useState(false);
 
+  const fetchComments = () => {
+    setIsLoading(true);
+    fetchArticleComments(article_id)
+      .then((response) => {
+        setComments(response);
+      })
+      .then(() => {
+        setIsLoading(false);
+      });
+  };
 
   useEffect(() => {
-    setIsLoading(true)
-    fetchArticleComments(article_id).then((response) => {
-        setComments(response)
-    }).then(() => {
-      setIsLoading(false)
-    })
-  }, [])
+    fetchComments();
+  }, [triggerFetch]);
 
   const commentsOnClickHandler = () => {
     if (showComments === "Show Comments") {
-        setShowComments("Hide Comments")
+      setShowComments("Hide Comments");
     } else {
-        setShowComments("Show Comments")
+      setShowComments("Show Comments");
     }
-  }
+  };
 
   return (
     <>
-      <h3>Comments ({ isLoading ? "..." : comments.length})</h3>
+      <h3>Leave a comment</h3>
+      <CommentForm article_id={article_id} setTriggerFetch={setTriggerFetch} />
+      <h3>Comments ({isLoading ? "..." : comments.length})</h3>
       <button onClick={commentsOnClickHandler}>{showComments}</button>
-      {showComments === "Hide Comments" ? comments.map((comment) => {return <CommentCard key={comment.author+comment.comment_id}comment={comment}/>}) : null}
+      {showComments === "Hide Comments"
+        ? comments.map((comment) => {
+            return (
+              <CommentCard
+                key={comment.author + comment.comment_id}
+                comment={comment}
+              />
+            );
+          })
+        : null}
     </>
   );
 };
--- src/utils/formatTimeStamp.js
+++ src/utils/formatTimeStamp.js
@@ -1,9 +1,12 @@
 import moment from "moment";
+import 'moment-timezone';
+
+moment.tz.setDefault("UTC");
 
 export const toDaysMonthsYears = (timeStamp) => {
     return moment(timeStamp).format('Do MMMM YYYY')
 }
 
 export const toRelativeTime = (timeStamp) => {
-    return moment(timeStamp, "YYYYMMDD").fromNow();
+    return moment(timeStamp).fromNow();
 }
