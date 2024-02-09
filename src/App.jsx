5d878e3bc1657f041bb560356bb3791580e633a6
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-02-09 11:02:26 +0000

    add error handling for SearchBar component, posting comments, and deleting comments

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import NavBar from './components/NavBar';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import MyAccount from './components/MyAccount';
+import NotFound from './components/NotFound';
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
       <Route path="/articles/topics/:topic_name" element={<Search />} />
       <Route path="/articles/:article_id" element={<ViewArticle />} />
       <Route path="/myaccount" element={<MyAccount />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
     <Footer/>
     </>
