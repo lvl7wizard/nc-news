085bb6b0064c8b41ac6fa0227ef5381b5af4ddb0
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-03-11 17:44:17 +0000

    updates styling on article cards

--- src/components/ArticleCardMini.jsx
+++ src/components/ArticleCardMini.jsx
@@ -1,11 +1,29 @@
 import { Link } from "react-router-dom";
 import { toDaysMonthsYears } from "../utils/formatTimeStamp";
+import styled from "styled-components";
+
+const Card = styled.div`
+  text-align: center;
+  background-color: rgb(255, 255, 255, 0.65);
+  border: solid black;
+  border-radius: 25px;
+  box-shadow: 7px 7px 2px 1px rgba(0, 0, 0, 0.226);
+  margin: 10px;
+  width: 300px;
+  color: black;
+  padding: 20px;
+
+  img {
+    width: 250px;
+  }
+`;
 
 const ArticleCardMini = ({ article }) => {
+
   const articleUrl = `/articles/${article.article_id}`;
   return (
     <Link className="link" to={articleUrl}>
-      <div className="article-card">
+      <Card>
         <h4>{article.title}</h4>
         <p>author: {article.author}</p>
         <img src={article.article_img_url} />
@@ -13,7 +31,7 @@ const ArticleCardMini = ({ article }) => {
         <p>Created: {toDaysMonthsYears(article.created_at)}</p>
         <p>Votes: {article.votes}</p>
         <p>Comments: {article.comment_count}</p>
-      </div>
+      </Card>
     </Link>
   );
 };
--- src/components/GlobalStyle.jsx
+++ src/components/GlobalStyle.jsx
@@ -3,7 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
   body {
     padding-top: 90px;
-    padding-bottom: 80px;
+    padding-bottom: 90px;
     margin: 0;
     font-family: Arial, Helvetica, sans-serif;
     background: linear-gradient(to bottom, #1c90bf, #eb8a3d);
