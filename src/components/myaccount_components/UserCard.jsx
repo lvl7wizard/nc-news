9356958a39e80a6145e4d34b50aa856f6a0eadef
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-03-11 20:36:04 +0000

    updates styling of user cards to use styled components

--- src/components/myaccount_components/UserCard.jsx
+++ src/components/myaccount_components/UserCard.jsx
@@ -1,5 +1,22 @@
 import { useContext} from 'react';
 import { UserContext } from '../../contexts/User';
+import styled from 'styled-components';
+const Card = styled.button`
+padding: 10px;
+margin: 10px;
+text-align: center;
+text-align: center;
+border: solid black;
+border-radius: 25px;
+box-shadow: 4px 4px 2px 1px rgba(0, 0, 0, 0.226);
+background-color: rgb(255, 255, 255, 0.65);
+
+img {
+    width: 100px;
+    max-height: 100px;
+    border-radius: 10px;
+  }
+`
 
 const UserCard = ({user}) => {
     const { setCurrentUser } = useContext(UserContext)
@@ -8,14 +25,10 @@ const UserCard = ({user}) => {
     }
 
     return (
-    <div className="user-card">
-        <button className="user-button" onClick={userOnClickHandler}>
+    <Card onClick={userOnClickHandler}>
         <img src={user.avatar_url}/>
-        <div>
         <p>{user.username}</p>
-        </div>
-        </button>
-    </div>)
+    </Card>)
 }
 
 export default UserCard; 
