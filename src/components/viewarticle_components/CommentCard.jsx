c4ba366cc1c38b4b399f163f80e4e368afa3896b
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-03-19 13:31:39 +0000

    refactors comment cards to use styled components. adds delete confirmation pop up

--- src/components/viewarticle_components/CommentCard.jsx
+++ src/components/viewarticle_components/CommentCard.jsx
@@ -3,6 +3,22 @@ import { deleteCommentById, fetchUserAvatar } from "../../utils/apiRequest";
 import { useEffect, useState } from "react";
 import { useContext } from "react";
 import { UserContext } from "../../contexts/User";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  display: block;
+  padding: 10px;
+  margin: 10px;
+  background: rgba(0, 0, 0, 0.7);
+  border-radius: 15px;
+  color: white;
+
+  img {
+    width: 50px;
+    max-height: 50px;
+    height: auto;
+  }
+`;
 
 const CommentCard = ({ setTriggerFetch, comment, comments, setComments }) => {
   const [userAvatar, setUserAvatar] = useState("");
@@ -21,43 +37,43 @@ const CommentCard = ({ setTriggerFetch, comment, comments, setComments }) => {
   }, []);
 
   const deleteOnClickHandler = () => {
-    const updatedComments = comments.filter((item) => {
-      return item.comment_id !== comment.comment_id;
-    })
-    setComments(updatedComments);
-    deleteCommentById(comment.comment_id)
-      .catch((error) => {
+    const isConfirmed = window.confirm(
+      "Are you sure you want to delete your comment?"
+    );
+    if (isConfirmed) {
+      const updatedComments = comments.filter((item) => {
+        return item.comment_id !== comment.comment_id;
+      });
+      setComments(updatedComments);
+      deleteCommentById(comment.comment_id).catch((error) => {
         window.alert(`Delete request was unsuccessful. Press okay to refresh.`);
         setTriggerFetch((prevTriggerFetch) => !prevTriggerFetch);
       });
+    }
   };
 
   if (isLoading) {
     return <p className="comment-card">Loading Comment...</p>;
   } else {
     return (
-      <div className="comment-card">
-        <div className="comment-card-1">
+      <StyledDiv>
+        <div>
           <img src={userAvatar} />
           <strong>{comment.author}</strong>{" "}
           <em>{toRelativeTime(comment.created_at)}</em>
         </div>
-        <div className="comment-card-1">
+        <div>
           <div>{comment.body}</div>
         </div>
-        <div className="comment-card-1">
+        <div>
           Votes = {comment.votes} <br />
         </div>
         {comment.author === currentUser.username ? (
-          <button
-            onClick={deleteOnClickHandler}
-          >
-            Delete comment
-          </button>
+          <button onClick={deleteOnClickHandler}>Delete comment</button>
         ) : (
           <></>
         )}
-      </div>
+      </StyledDiv>
     );
   }
 };
