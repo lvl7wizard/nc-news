4b09f86440f995dd51b39253f659010669370845
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-04-02 15:22:47 +0100

    styles ArticleCardFull using styled components

--- src/components/viewarticle_components/ArticleCardFull.jsx
+++ src/components/viewarticle_components/ArticleCardFull.jsx
@@ -3,6 +3,49 @@ import { toDaysMonthsYears } from "../../utils/formatTimeStamp";
 import { patchArticleLikes } from "../../utils/apiRequest";
 import { useContext } from "react";
 import { UserContext } from "../../contexts/User";
+import styled from "styled-components";
+
+const ArticleContainer = styled.div`
+  display: block;
+  width: 90%;
+  margin: auto;
+  color: white;
+  padding: 10px;
+  background: rgba(0, 0, 0, 0.7);
+  border-radius: 15px;
+`;
+
+const ArticleTitle = styled.h2`
+  text-align: center;
+`;
+
+const ArticleInfoContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  margin: auto;
+`;
+
+const ArticleImage = styled.img`
+  width: 100%;
+  height: auto;
+`;
+
+const ArticleBody = styled.div`
+  text-align: center;
+`;
+
+const VotingContainer = styled.div`
+margin-top: 15px;
+border-width: thin;
+text-align: center;
+`
+
+const VotingButton = styled.button`
+margin: 10px;
+margin-top: 0px;
+margin-bottom: 0px;
+`
 
 // This component currently uses local storage to check whether a user has already voted for an article
 // as the backend does not currently support storing this data
@@ -20,14 +63,7 @@ const ArticleCardFull = ({ article }) => {
   }
 
   const likeOnClickHandler = (event) => {
-    listOfArticleVotes.push(article.article_id);
-    localStorage.setItem(
-      currentUser.username,
-      JSON.stringify(listOfArticleVotes)
-    );
-
-    
-    let increment = event.target.id === "like" ? 1 : -1
+    let increment = event.target.id === "like" ? 1 : -1;
     setCurrentVotes((currentVotes) => currentVotes + increment);
     patchArticleLikes(article.article_id, increment).then((response) => {
       if (response.name === "AxiosError") {
@@ -35,49 +71,72 @@ const ArticleCardFull = ({ article }) => {
         setCurrentVotes((currentVotes) => currentVotes - increment);
       } else {
         setErrorMessage(null);
+        listOfArticleVotes.push(article.article_id);
+        localStorage.setItem(
+          currentUser.username,
+          JSON.stringify(listOfArticleVotes)
+        );
+
       }
     });
   };
 
   return (
-    <main className="full-article">
-      <h2>{article.title}</h2>
-      <div>
-        <p>author: {article.author}</p>
-        <p>created: {toDaysMonthsYears(article.created_at)}</p>
-        <p>topic = {article.topic}</p>
-      </div>
-      <img src={article.article_img_url} />
-      <p>{article.body}</p>
-      <div className="votes">
-        <p>Votes:</p>
-        {errorMessage ? (
-          <p className="error-text">Error: "{errorMessage}"</p>
-        ) : null}
-        <p>{currentVotes}</p>
-        <button
-          id="like"
-          onClick={likeOnClickHandler}
-          disabled={
-            listOfArticleVotes.includes(article.article_id) ? true : false
-          }
-        >
-          👍
-        </button>
-        <button
-          id="dislike"
-          onClick={likeOnClickHandler}
-          disabled={
-            listOfArticleVotes.includes(article.article_id) ? true : false
-          }
-        >
-          👎
-        </button>
-        {listOfArticleVotes.includes(article.article_id) ? (
-          <p>Thanks for voting!</p>
-        ) : null}
-      </div>
-    </main>
+    <>
+    <ArticleContainer>
+      <ArticleTitle>{article.title}</ArticleTitle>
+      <ArticleInfoContainer>
+        <p>
+          author: <span className="bolded">{article.author}</span>
+        </p>
+        <p>
+          topic: <span className="bolded">{article.topic}</span>
+        </p>
+        <p>
+          created:{" "}
+          <span className="bolded">
+            {toDaysMonthsYears(article.created_at)}
+          </span>
+        </p>
+        <p>
+          Votes: <span className="bolded">{currentVotes}</span>
+        </p>
+      </ArticleInfoContainer>
+      <ArticleImage src={article.article_img_url} />
+      <ArticleBody>
+        <p>{article.body}</p>
+      </ArticleBody>
+    </ArticleContainer>
+          <VotingContainer>
+          <p>Did you enjoy this article?</p>
+            <VotingButton
+              id="like"
+              onClick={likeOnClickHandler}
+              disabled={
+                listOfArticleVotes.includes(article.article_id) ? true : false
+              }
+              >
+              👍
+            </VotingButton>
+            <VotingButton
+              id="dislike"
+              onClick={likeOnClickHandler}
+              disabled={
+                listOfArticleVotes.includes(article.article_id) ? true : false
+              }
+              >
+              👎
+            </VotingButton>
+            {listOfArticleVotes.includes(article.article_id) ? (
+              <p>Thanks for voting!</p>
+              ) : (
+                null
+                )}
+                {errorMessage ? (
+                  <p className="error-text">Error: "{errorMessage}"</p>
+                ) : null}
+          </VotingContainer>
+                </>
   );
 };
 
