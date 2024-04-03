12906c1d434274c559c3082cf076aff5c194d987
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-04-03 13:22:17 +0100

    change styling of user cards to match app theme

--- src/components/myaccount_components/UserCard.jsx
+++ src/components/myaccount_components/UserCard.jsx
@@ -1,6 +1,6 @@
-import { useContext } from 'react';
-import { UserContext } from '../../contexts/User';
-import styled from 'styled-components';
+import { useContext } from "react";
+import { UserContext } from "../../contexts/User";
+import styled from "styled-components";
 
 const Card = styled.button`
   padding: 10px;
@@ -9,12 +9,20 @@ const Card = styled.button`
   border: solid black;
   border-radius: 25px;
   box-shadow: 4px 4px 2px 1px rgba(0, 0, 0, 0.226);
-  background-color: rgb(255, 255, 255, 0.65);
+  background-color: rgb(0, 0, 0, 0.65);
+  color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
   img {
+    border: solid black;
+    border-radius: 15px;
     width: 100px;
+    min-height: 100px;
     max-height: 100px;
-    border-radius: 10px;
+    height: auto;
+    background: white;
   }
 `;
 
@@ -26,7 +34,16 @@ const UserCard = ({ user }) => {
   };
 
   return (
-    <Card onClick={userOnClickHandler} style={{ backgroundColor: user.username === currentUser.username ? 'lightblue' : null }}>
+    <Card
+      onClick={userOnClickHandler}
+      style={{
+        backgroundColor:
+          user.username === currentUser.username
+            ? "rgba(0, 200, 0, 0.5)"
+            : null,
+        color: user.username === currentUser.username ? "black" : null,
+      }}
+    > 
       <img src={user.avatar_url} alt={user.username} />
       <p>{user.username}</p>
     </Card>
