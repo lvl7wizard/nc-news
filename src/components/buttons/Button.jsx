163cf94455a4a8ce15b792c96bd7ccd42d742a85
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-04-04 23:57:53 +0100

    add conditional styling for disabled prop

--- src/components/buttons/Button.jsx
+++ src/components/buttons/Button.jsx
@@ -1,14 +1,17 @@
 import styled from "styled-components";
 
 const Button = styled.button`
+background-color: rgba(0, 0, 0, 0.7);
   color: white;
-  background-color: rgba(0, 0, 0, 0.7);
   border-radius: 25px;
   border: none;
+  opacity: ${(props) => (props.disabled ? 0.5 : 1)};
+  cursor: ${(props) => (props.disabled ? "not-allowed" : "pointer")};
+  transition: opacity 0.2s ease;
+  margin-left: 10px;
   margin-right: 10px;
   padding: 10px;
-  cursor: pointer;
-
+  
   &:hover {
     outline: solid white;
     border-radius: 25px;
