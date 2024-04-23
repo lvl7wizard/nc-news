42e55ce68b387ff1dfce88c102b441fc5b3cb8c4
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-04-23 14:07:33 +0100

    adjusts background and position of search bar

--- src/components/forms/StyledForm.jsx
+++ src/components/forms/StyledForm.jsx
@@ -4,15 +4,21 @@ const StyledForm = styled.form`
 display: flex;
 flex-wrap: wrap;
 justify-content: center;
-margin-left: 10vw;
-margin-right: 10vw;
-margin-top: 10px;
+// margin-left: 10vw;
+// margin-right: 10vw;
+// margin-top: 10px;
 margin-bottom: 10px;
 padding: 10px;
 gap: 10px;
 
-background: rgba(0, 0, 0, 0.7);
-border-radius: 15px;
+background: rgba(0, 0, 0, 0.6);
+
+select {
+    border: none;
+    border-radius: 4px;
+    background-color: black;
+    color: white;
+  }
 
 color: white;
 `
