f5e4e239a112123a37e0bfffb27eaefcd73937f7
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-05-15 16:57:46 +0100

    refactors PostForm using Bootstrap

--- src/components/pages/Post/Post.jsx
+++ src/components/pages/Post/Post.jsx
@@ -2,20 +2,24 @@ import { useContext } from "react";
 import styles from "./Post.module.css";
 import { UserContext } from "../../../contexts/User";
 import PostForm from "./PostForm.jsx/PostForm";
-import Button from "react-bootstrap/Button"
-
+import Button from "react-bootstrap/Button";
 
 const Post = () => {
   const { currentUser } = useContext(UserContext);
   return (
     <div className={styles.container}>
-      {currentUser === null ?         <>
-        <h2>You must be logged in to post articles</h2>
-        <Button href="/">Log in</Button>
-        </> : <PostForm/>}
+      {currentUser === null ? (
+        <div className={styles.notLoggedInText}>
+          <h2>You must be logged in to post articles</h2>
+          <Button href="/">Log in</Button>
+        </div>
+      ) : (
+        <div>
+          <PostForm />
+        </div>
+      )}
     </div>
-  )
-}
-
+  );
+};
 
 export default Post;
