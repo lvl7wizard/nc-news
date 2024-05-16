7e68a685b0307207a542264f4e144ffab0c212c0
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-05-16 23:03:48 +0100

    replaces inline styling with a styled component

--- src/components/pages/NotFound/NotFound.jsx
+++ src/components/pages/NotFound/NotFound.jsx
@@ -1,8 +1,20 @@
+import styled from "styled-components";
+
+const NotFoundContainer = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+text-align: center;
+height: calc(100vh - 60px);
+min-height: 150px;
+`;
+
 const NotFound = () => {
-    return (<div>
-<h3 align="center">404 - Not Found</h3>
-<p align="center">Sorry, the page you are looking for does not exist.</p>
-    </div>
+    return (<NotFoundContainer>
+<h3>404 - Not Found</h3>
+<p>Sorry, the page you are looking for does not exist.</p>
+    </NotFoundContainer>
 
     )
 }
