61cc2c5d4ac8e2a063fa6125de9feb6b058706a2
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-05-23 11:27:49 +0100

    adjusts styling to match site theme

--- src/components/pages/ViewArticle/CommentSection/CommentCard/CommentCard.jsx
+++ src/components/pages/ViewArticle/CommentSection/CommentCard/CommentCard.jsx
@@ -3,14 +3,11 @@ import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faHeart, faTrash, faAt } from "@fortawesome/free-solid-svg-icons";
 import { toRelativeTime } from "../../../../../utils/formatTimeStamp";
-import {
-  deleteCommentById,
-} from "../../../../../utils/apiRequest";
+import { deleteCommentById } from "../../../../../utils/apiRequest";
 import { UserContext } from "../../../../../contexts/User";
 import ErrorMessage from "../../../../modals/ErrorMessage";
-
 
 const CommentCard = ({
   setTriggerFetch,
@@ -23,7 +20,7 @@ const CommentCard = ({
 }) => {
   const { currentUser } = useContext(UserContext);
   const [showErrorModal, setShowErrorModal] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("")
+  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
     console.log("showErrorModal state changed:", showErrorModal);
@@ -32,11 +29,17 @@ const CommentCard = ({
   const deleteComment = async () => {
     try {
       await deleteCommentById(comment.comment_id);
-      const updatedComments = comments.filter((item) => item.comment_id !== comment.comment_id);
+      const updatedComments = comments.filter(
+        (item) => item.comment_id !== comment.comment_id
+      );
       setComments(updatedComments);
     } catch (error) {
       console.error(error);
-      setErrorMessage(`Error: ${error.response ? error.response.data.msg : error.message}. Please refresh and try again.`);
+      setErrorMessage(
+        `Error: ${
+          error.response ? error.response.data.msg : error.message
+        }. Please refresh and try again.`
+      );
       setShowErrorModal(true);
     }
   };
@@ -47,59 +50,71 @@ const CommentCard = ({
     setDeleteFunction(() => deleteComment);
   };
 
-    return (
-      <>
-      <ErrorMessage showModal={showErrorModal} setShowErrorModal={setShowErrorModal} errorMessage={errorMessage}/>
+  return (
+    <>
+      <ErrorMessage
+        showModal={showErrorModal}
+        setShowErrorModal={setShowErrorModal}
+        errorMessage={errorMessage}
+      />
       <Card
-        className="w-100 bg-secondary text-black"
+        className="w-100 border-white border-opacity-50 bg-secondary bg-opacity-25 text-light"
         style={{ maxWidth: "1000px" }}
       >
         <Card.Body>
-          <Row>
-            <Col xs={10}>
-              <Card.Title>{comment.author}</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">
+          <Row className="mb-2">
+            <Col xs={6}>
+              <Card.Title>
+                <FontAwesomeIcon
+                  icon={faAt}
+                  color="lightblue"
+                  aria-label="author icon"
+                  title="author:"
+                />{" "}
+                {comment.author}
+              </Card.Title>
+            </Col>
+            <Col xs={6} className="text-end">
+              <Card.Subtitle className="text-primary">
                 {toRelativeTime(comment.created_at)}
               </Card.Subtitle>
+            </Col>
+          </Row>
+          <Row className="mb-3">
+            <Col>
               <Card.Text>{comment.body}</Card.Text>
-              <Card.Text className="mb-2 text-muted">
-                {" "}
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={10}>
+              <Card.Text className="mb-2 text-light">
                 <FontAwesomeIcon
                   icon={faHeart}
-                  color="black"
+                  color="pink"
                   aria-label="likes icon"
                   title="likes:"
                 />{" "}
                 {comment.votes}
               </Card.Text>
             </Col>
-            <Col
-              xs={2}
-              className="d-flex justify-content-center align-items-end"
-            >
-              {currentUser !== null ? (
-                comment.author === currentUser.username ? (
+            <Col xs={2} className="text-end">
+              {currentUser !== null &&
+                comment.author === currentUser.username && (
                   <FontAwesomeIcon
                     icon={faTrash}
-                    color="black"
+                    color="red"
                     aria-label="delete icon"
                     title="delete comment"
                     onClick={deleteOnClickHandler}
                     size="xl"
-                    style={{ marginBottom: "8px" }}
                   />
-                ) : (
-                  <></>
-                )
-              ) : (
-                <></>
-              )}
+                )}
             </Col>
           </Row>
         </Card.Body>
       </Card>
-      </>
-    );
-  };
+    </>
+  );
+};
 
 export default CommentCard;
