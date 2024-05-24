e85f2d728eab3b9588abbbd31a475a49ad8ce93c
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-05-24 16:32:23 +0100

    replaces module.css with styled component

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,28 +1,36 @@
 import { useState, useContext, useEffect } from "react";
-import styles from "./Home.module.css";
 import Loading from "../../loading/Loading";
 import Login from "./Login/Login";
 import WelcomeMessage from "./WelcomeMessage/WelcomeMessage";
 import { fetchUsers } from "../../../utils/apiRequest";
 import { UserContext } from "../../../contexts/User";
+import styled from "styled-components";
+
+const HomeContainer = styled.div`
+display: flex;
+flex-direction: column;
+height: calc(100vh - 60px);
+align-items: center;
+justify-content: center;
+`
 
 const Home = () => {
   const [isLoading, setIsLoading] = useState(false);
   const { currentUser } = useContext(UserContext);
 
   useEffect(() => {
-    // API call added on render just to wake up the server
+    // API call added on render just to wake up the server as it can take up to a minute
     fetchUsers();
   }, []);
 
   return (
-    <div className={styles.container}>
+    <HomeContainer>
       {currentUser === null ? (
         isLoading ? <Loading/> : <Login setIsLoading={setIsLoading}/>
       ) : (
         <WelcomeMessage/>
       )}
-    </div>
+    </HomeContainer>
   );
 };
 
