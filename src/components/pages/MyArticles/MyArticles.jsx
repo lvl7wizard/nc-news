74c8bf2399b454b636730276c4fff05b2fabbf2c
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-05-24 15:42:42 +0100

    refactor: text from MyArticleCards moved to MyArticles replaces module.css with styled component

--- src/components/pages/MyArticles/MyArticles.jsx
+++ src/components/pages/MyArticles/MyArticles.jsx
@@ -1,24 +1,41 @@
 import { UserContext } from "../../../contexts/User";
 import { useContext } from "react";
-import styles from "./MyArticles.module.css";
 import Button from "react-bootstrap/Button";
-
 import MyArticleCards from "./MyArticlesCards";
+import styled from "styled-components";
+
+const MyArticlesContainer = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+text-align: center;
+min-height: calc(100vh - 60px);
+margin-left: 5vw;
+margin-right: 5vw;
+`
 
 const MyArticles = () => {
   const { currentUser } = useContext(UserContext);
 
   return (
-    <div className={styles.container}>
+    <MyArticlesContainer>
       {currentUser === null ? (
         <>
           <h2>You must be logged in to view your articles</h2>
           <Button href="/">Log in</Button>
         </>
       ) : (
+        <>
+        <p>
+          Click the buttons below to <span className="text-success">view</span>,{" "}
+          <span className="text-warning">edit</span>, or{" "}
+          <span className="text-danger">delete</span> your articles
+        </p>        
         <MyArticleCards />
+        </>
       )}
-    </div>
+    </MyArticlesContainer>
   );
 };
 
