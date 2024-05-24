6eee9a5fd3e5e1a6902523b3b7eb5a23925923a6
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-05-24 16:27:36 +0100

    replaces module.css with styled components, adds aria label for ReactTyped component

--- src/components/pages/Home/WelcomeMessage/WelcomeMessage.jsx
+++ src/components/pages/Home/WelcomeMessage/WelcomeMessage.jsx
@@ -1,26 +1,54 @@
-import { ReactTyped } from "react-typed";
-import styles from "./WelcomeMessage.module.css";
 import { useContext } from "react";
 import { UserContext } from "../../../../contexts/User";
+import styled from "styled-components";
+import { ReactTyped } from "react-typed";
+
+const WelcomeMessageContainer = styled.div`
+display: flex;
+flex-wrap: wrap;
+flex-direction: column;
+gap: 5vh;
+margin-left: 10vw;
+margin-right: 10vw;
+text-align: center;
+`
+
+const MainText = styled.h2`
+font-size: 80px;
+@media (max-width: 640px) {
+      font-size: 50px;
+}
+`
+
+const UserNameText = styled.span`
+background-color: #f3ec78;
+background-image: linear-gradient(180deg, #f3ef96 30%, #ffbb1d);
+background-size: 100%;
+-webkit-background-clip: text;
+-moz-background-clip: text;
+-webkit-text-fill-color: transparent;
+-moz-text-fill-color: transparent;
+background-clip: text;
+text-shadow: 0 0 40px rgba(255, 228, 96, 0.2);
+`
 
 const WelcomeMessage = () => {
   const { currentUser } = useContext(UserContext);
   return (
-    <div className={styles.content}>
-      <h2 className={styles.welcomeMessage}>
+    <WelcomeMessageContainer>
+      <MainText>
         Welcome back,{" "}
-        <span className={styles.username}>{currentUser.username}</span>
-      </h2>
-      <div className={styles.text}>
+        <UserNameText>{currentUser.username}</UserNameText>
+      </MainText>
         <ReactTyped
           strings={[
             "Hit up the menu to start reading, commenting, and posting!",
           ]}
           typeSpeed={50}
           loop={false}
+          aria-label="Instructions: Hit up the menu to start reading, commenting, and posting!"
         />
-      </div>
-    </div>
+    </WelcomeMessageContainer>
   );
 };
 
