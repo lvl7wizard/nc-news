605744ea97763ecc43677115ed94295a0b54dea0
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-05-24 13:49:45 +0100

    moves text and buttons from ChangeAccount into MyAccount

--- src/components/pages/MyAccount/MyAccount.jsx
+++ src/components/pages/MyAccount/MyAccount.jsx
@@ -1,30 +1,66 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../../../contexts/User";
-import ChangeAccount from "./ChangeAccount.jsx/ChangeAccount";
+import AccountsTable from "./AccountsTable/AccountsTable";
 import Button from "react-bootstrap/Button";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 const MyAccountContainer = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-text-align: center;
-height: calc(100vh - 60px);
-min-height: 150px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: calc(100vh - 60px);
+  justify-content: center;
 `;
 
+const ButtonContainer = styled.div`
+display: flex;
+gap: 20px;
+margin-bottom: 20px;
+`
+
 const MyAccount = () => {
-  const { currentUser } = useContext(UserContext);
+  const { currentUser, setCurrentUser } = useContext(UserContext);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const logOut = () => {
+    setCurrentUser(null);
+    navigate(`/`);
+  };
+
+  const [showAccountsTable, setShowAccountsTable] = useState(false);
+  const onClickChangeAccount = () => {
+    setShowAccountsTable(!showAccountsTable);
+  };
+
   return (
     <MyAccountContainer>
       {currentUser === null ? (
         <>
-        <h2>You must be logged in to view your account</h2>
-        <Button href="/">Log in</Button>
+          <h2>You must be logged in to view your account</h2>
+          <Button href="/">Log in</Button>
         </>
       ) : (
-        <ChangeAccount />
+        <>
+          <p>
+            You are currently logged in as <mark>{currentUser.username}</mark>
+          </p>
+          <ButtonContainer>
+          <Button onClick={logOut}>Log out</Button>
+          <Button
+            onClick={onClickChangeAccount}
+            disabled={isLoading ? true : false}
+          >
+            {!isLoading ? "Change account" : "Loading..."}
+          </Button>
+          </ButtonContainer>
+          {showAccountsTable ? (
+            <AccountsTable isLoading={isLoading} setIsLoading={setIsLoading} />
+          ) : (
+            <></>
+          )}
+        </>
       )}
     </MyAccountContainer>
   );
