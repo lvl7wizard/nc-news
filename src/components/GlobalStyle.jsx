ba6719e2a216500809b0a7a8e23777c53c07d359
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-05-24 17:25:35 +0100

    removes redundant margin: 0 property

--- src/components/GlobalStyle.jsx
+++ src/components/GlobalStyle.jsx
@@ -3,7 +3,6 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
   body {
     padding-top: 60px;
-    margin: 0;
     color: white;
     background: black;
     background-attachment: fixed;
