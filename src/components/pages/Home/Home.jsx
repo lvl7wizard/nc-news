893e1f7ce34275d27c7c854fd3a7bfc7503cc38e
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-06-11 12:41:31 +0100

    remove unnecessary fetchUsers API call on Home component render

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,34 +1,32 @@
-import { useState, useContext, useEffect } from "react";
+import { useState, useContext } from "react";
 import Loading from "../../loading/Loading";
 import Login from "./Login/Login";
 import WelcomeMessage from "./WelcomeMessage/WelcomeMessage";
-import { fetchUsers } from "../../../utils/apiRequest";
 import { UserContext } from "../../../contexts/User";
 import styled from "styled-components";
 
 const HomeContainer = styled.div`
-display: flex;
-flex-direction: column;
-height: calc(100vh - 60px);
-align-items: center;
-justify-content: center;
-`
+  display: flex;
+  flex-direction: column;
+  height: calc(100vh - 60px);
+  align-items: center;
+  justify-content: center;
+`;
 
 const Home = () => {
   const [isLoading, setIsLoading] = useState(false);
   const { currentUser } = useContext(UserContext);
 
-  useEffect(() => {
-    // API call added on render just to wake up the server as it can take up to a minute
-    fetchUsers();
-  }, []);
-
   return (
     <HomeContainer>
       {currentUser === null ? (
-        isLoading ? <Loading/> : <Login setIsLoading={setIsLoading}/>
+        isLoading ? (
+          <Loading />
+        ) : (
+          <Login setIsLoading={setIsLoading} />
+        )
       ) : (
-        <WelcomeMessage/>
+        <WelcomeMessage />
       )}
     </HomeContainer>
   );
