e487dce43b5414d548f27d5f268e7c52df6a9ba6
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-06-19 20:59:42 +0100

    styles Login with dark background, adds rocket image

--- src/components/pages/Home/Login/Login.jsx
+++ src/components/pages/Home/Login/Login.jsx
@@ -10,6 +10,14 @@ import FormText from "react-bootstrap/FormText";
 import { useContext } from "react";
 import { UserContext } from "../../../../contexts/User";
 import { fetchUser } from "../../../../utils/apiRequest";
+import rocketIcon from "../../../../assets/Login/rocketIcon.png"
+import styled from "styled-components";
+
+const LoginImage = styled.img`
+  width: 100px;
+  height: 100px;
+`;
+
 
 let Login = ({setIsLoading}) => {
 const { setCurrentUser } = useContext(UserContext);
@@ -65,69 +73,73 @@ const { setCurrentUser } = useContext(UserContext);
   };
 
   return (
-    <Container>
-      <Row className="d-flex justify-content-center">
-        <Col xs="auto">
-          <Card className="shadow-lg">
-            <Card.Header
-              className="text-center pt-3 pb-3 rounded-top bg-secondary text-white border-bottom-0"
-            >
-              <h2 className="mb-0" style={{textShadow:"2px 2px black"}}>Member Login</h2>
-            </Card.Header>
-            <Card.Body className="text-dark rounded-bottom bg-secondary">
-              <Form onSubmit={handleSubmit}>
-                <Form.Group className="mb-3" controlId="formUsername">
-                  <FloatingLabel controlId="floatingUsername" label="Username">
-                    <Form.Control
-                      required
-                      name="username"
-                      onChange={updateUsername}
-                      type="text"
-                      placeholder="Username"
-                      aria-describedby="usernameHelpBlock"
-                      isInvalid={usernameInvalid}
-                      isValid={usernameValid}
-                    ></Form.Control>
-                    <FormText id="usernameHelpBlock" className="text-dark">
-                      Use 'grumpy19', 'jessjelly', or 'happyamy2016'
-                    </FormText>
-                    <Form.Control.Feedback type="invalid">
-                      {usernameFeedback}
-                    </Form.Control.Feedback>
-                  </FloatingLabel>
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="formPassword">
-                  <FloatingLabel controlId="floatingPassword" label="Password">
-                    <Form.Control
-                      required
-                      name="password"
-                      onChange={updatePassword}
-                      type="password"
-                      placeholder="Password"
-                      aria-describedby="passwordHelpBlock"
-                    />
-                    <Form.Control.Feedback type="invalid">
-                      Password cannot be blank.
-                    </Form.Control.Feedback>
-                    <Form.Text id="passwordHelpBlock" className="text-dark">
-                      Type anything for the password.
-                    </Form.Text>
-                  </FloatingLabel>
-                </Form.Group>
-
-                <Form.Group className="text-center">
-                  <Button variant="primary" type="submit">
-                    Login
-                  </Button>
-                </Form.Group>
-              </Form>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
-  );
-};
+      <Container>
+        <Row className="d-flex justify-content-center">
+          <Col xs="auto">
+            <Card className="shadow-lg bg-dark bg-opacity-75 text-light">
+              <Card.Header className="text-center rounded-top border-bottom-0">
+              <LoginImage src={rocketIcon} alt="a cartoon rocket with a circular blue window in launch"/>
+                <h2 className="" style={{ textShadow: "2px 2px black" }}>
+                  Sign In
+                </h2>
+              </Card.Header>
+              <Card.Body className="text-light rounded-bottom">
+                <Form onSubmit={handleSubmit}>
+                  <Form.Group className="mb-3" controlId="formUsername">
+                    <FloatingLabel controlId="floatingUsername" label="Username">
+                      <Form.Control
+                        required
+                        name="username"
+                        onChange={updateUsername}
+                        type="text"
+                        placeholder="Username"
+                        aria-describedby="usernameHelpBlock"
+                        isInvalid={usernameInvalid}
+                        isValid={usernameValid}
+                        className="bg-dark text-light"
+                      ></Form.Control>
+                      <FormText id="usernameHelpBlock" className="text-white">
+                        You can use 'grumpy19' for this demo
+                      </FormText>
+                      <Form.Control.Feedback type="invalid">
+                        {usernameFeedback}
+                      </Form.Control.Feedback>
+                    </FloatingLabel>
+                  </Form.Group>
+  
+                  <Form.Group className="mb-3" controlId="formPassword">
+                    <FloatingLabel controlId="floatingPassword" label="Password">
+                      <Form.Control
+                        required
+                        name="password"
+                        onChange={updatePassword}
+                        type="password"
+                        placeholder="Password"
+                        aria-describedby="passwordHelpBlock"
+                        className="bg-dark text-light"
+                      />
+                      <Form.Control.Feedback type="invalid">
+                        Password cannot be blank.
+                      </Form.Control.Feedback>
+                      <Form.Text id="passwordHelpBlock" className="text-white">
+                        Type anything for the password
+                      </Form.Text>
+                    </FloatingLabel>
+                  </Form.Group>
+  
+                  <Form.Group className="text-center">
+                    <Button variant="primary" type="submit">
+                      Login
+                    </Button>
+                  </Form.Group>
+                </Form>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    );
+  };
+  
 
 export default Login;
